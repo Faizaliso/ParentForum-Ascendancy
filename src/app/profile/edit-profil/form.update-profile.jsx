747280996b3491bbe01efc5d64630511f0e97cf3
arifskd <arifskd@gmail.com>
2024-07-28 18:10:14 +0700

    Update form.update-profile.jsx

--- src/app/profile/edit-profil/form.update-profile.jsx
+++ src/app/profile/edit-profil/form.update-profile.jsx
@@ -1,28 +1,31 @@
 "use client";
 
-// import { useActionState } from "react";
+import { useActionState } from "react";
 import { logoutAction } from "./action";
+import { updateProfileAction } from "./action";
 
 export const UpdateUserForm = ({ user }) => {
-  // const [state, formAction, pending] = (useActionState, null);
+  const [state, formAction, pending] = useActionState(updateProfileAction, null);
   return (
     <main>
-      <form className="space-y-2">
+      <form action={formAction} className="space-y-2">
+        <input type="hidden" name="id" defaultValue={user?.id} />
         <input name="name" placeholder="name" defaultValue={user?.name} />
         <input
           name="email"
           type="email"
           placeholder="email"
           defaultValue={user?.email}
+          disabled
         />
 
         <button>Save Profile</button>
-        {/* {state?.status === "error" && (
-        <p className="text-red-500">{state.message}</p>
-      )}
-      {state?.status === "success" && (
-        <p className="text-emerald-500">{state.message}</p>
-      )} */}
+        {state?.status === "error" && (
+          <p className="text-red-500">{state.message}</p>
+        )}
+        {state?.status === "success" && (
+          <p className="text-emerald-500">{state.message}</p>
+        )}
       </form>
       <form action={logoutAction}>
         <button>Log Out</button>
